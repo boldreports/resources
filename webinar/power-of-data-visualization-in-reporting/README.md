97c7f988c9e191f7dbd1b38f2f7bc804ef4039ea
Subhasree Vasudevan <subhasree.vasudevan@syncfusion.com>
2021-06-02 19:07:00 +0530

    feature(REPORT-7676): Webinar assets

---
+++ webinar/power-of-data-visualization-in-reporting/README.md
@@ -0,0 +1,6 @@
+# Bold Report Webinar - Power of Data Visualization in Reporting
+
+This will contain the RDL report files which is created during the webinar and sql file which contains the information of data source and dataset.
+
+* [Documentation](https://help.boldreports.com/)
+* [Blog](https://www.boldreports.com/blog/)
---
+++ webinar/power-of-data-visualization-in-reporting/ticket-sales-analysis-sql-file.sql
@@ -0,0 +1,45 @@
+--
+-- Database: `ReportSampleDatabase`
+--
+
+--
+-- Connection String
+-- Data Source=dataplatformdemodata.syncfusion.com;Initial Catalog=SampleDB;user id=demoreadonly@data-platform-demo;password=N@c)=Y8s*1&dh
+--
+
+--
+-- Authentication Type
+-- None
+--
+
+-- --------------------------------------------------------
+
+--
+-- Dataset `ShowDetails`
+--
+
+SELECT [dbo].[Theater].[ShowDate],
+[dbo].[Theater].[ShowStartDate],
+[dbo].[Theater].[ShowName],
+[dbo].[Theater].[BoxName],
+[dbo].[Theater].[TicketsSold],
+[dbo].[Theater].[UnitPrice] FROM [dbo].[Theater]
+
+--
+-- Dataset `TopShowDetails`
+--
+
+SELECT [dbo].[Theater].[ShowDate],
+[dbo].[Theater].[ShowStartDate],
+[dbo].[Theater].[ShowName],
+[dbo].[Theater].[BoxName],
+Sum([dbo].[Theater].[TicketsSold]) as TicketsSold,
+Sum([dbo].[Theater].[UnitPrice]) as UnitPrice FROM [dbo].[Theater] Group by [dbo].[Theater].[ShowName],[dbo].[Theater].[BoxName],[dbo].[Theater].[ShowDate],[dbo].[Theater].[ShowStartDate]
+
+--
+-- Dataset `TopShows`
+--
+
+SELECT 
+[dbo].[Theater].[ShowName],
+Sum([dbo].[Theater].[TicketsSold]) as TotalTicketsSold FROM [dbo].[Theater] Group by [dbo].[Theater].[ShowName]
