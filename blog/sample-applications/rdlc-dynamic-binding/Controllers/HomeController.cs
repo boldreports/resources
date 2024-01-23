a6c243b02c570dccf4630c80055d039f4705a5c6
umamageswari-e <umamageswari.e@syncfusion.com>
2024-01-23 10:56:01 +0530

    documentation(REPORT-338652): Renamed the application name

---
+++ blog/sample-applications/rdlc-dynamic-binding/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RDLCDynamicBinding.Models;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace RDLCDynamicBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMemoryCache _cache;
+        private IWebHostEnvironment _hostingEnvironment;
+
+        public HomeController(IMemoryCache memoryCache, IWebHostEnvironment hostingEnvironment)
+        {
+            _cache = memoryCache;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewReport(int productId)
+        {
+            ViewBag.ProductID = productId;
+   
+            // Pass the product data to the view
+            return View("ViewReport");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
